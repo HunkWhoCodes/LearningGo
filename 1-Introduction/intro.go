55302cc39d170f44b2067ab290367552115d395e
Alok <hunkwhocodes@gmail.com>
2022-10-23 13:34:48 +0530

    Learning GO

---
+++ 1-Introduction/intro.go
@@ -0,0 +1,30 @@
+// Go:
+
+// GO is a compiled, statically typed language.
+// GO tool can run file without precompiling, but in the background the application is precompiled to a temporary executable file
+// Compiled files only run on a single OS unlike Java (no byte code)
+
+// Applications built with GO have a statically linked runtime component that's packaged with the application during the compilation process.
+// So the package is much larger than the source code file.
+// There is no external virtual machine like Java which has an OS specific virtual machine.
+// So java application compiled into byte code can be very very small but not in GO
+
+// Is GO Object Oriented? Sort of, not really
+
+// Sort of, can implement custom interfaces.
+// Custom types have methods
+// Custom structs can have member methods or member fields.
+
+// What GO Does not support?
+
+// There is no inheritance in GO. So no super type or no subtype
+// No concept of classes ion GO either
+// No method or operator overloading
+// Does not have structured exception handling
+// No implicit numeric conversion, so must type explicit variable types and conversions
+
+// Related languages
+
+// Borrows a lot from C
+// Tries to reduce typing
+
